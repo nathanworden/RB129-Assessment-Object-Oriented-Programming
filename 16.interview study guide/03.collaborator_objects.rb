9718bf590406cc7c9a2f40a4b829d426efc32a19
Nathan Worden <worden.nathan@gmail.com>
2019-08-30 05:30:52 -0700

    Preperation for Interview portion of RB129

--- 16.interview study guide/03.collaborator_objects.rb
+++ 16.interview study guide/03.collaborator_objects.rb
@@ -1,6 +1,8 @@
-# What is a collaborator object and why is it useful.
+# What is a collaborator object and why is it useful?
 
 class Course
+  attr_accessor :teacher
+
   def initialize(teacher, students)
     @teacher = teacher
     @students = []
@@ -9,23 +11,26 @@ end
 
 
 class Teacher
+  attr_reader :name
+
   def initialize(name)
     @name = name
   end
 end
 
-
 mr_bill = Teacher.new('Bill')
 physics = Course.new(mr_bill, [])
 
+p physics.teacher.name
 
-# Objects that are stored as state within another object are also called 
+
+# Objects that are stored as state within another 
+# object are also called 
 # "collaborator objects".
 
 
-# Collaborator objects represent the connections between various actors 
-# in your program. 
+# Collaborator objects represent the connections 
+# between various actors in your program. 
 
-
-# Collaborator objects allow you to chop up and modularize the problem 
-# domain into cohesive pieces
+# Collaborator objects allow you to chop up and 
+# modularize the problem domain into cohesive pieces
--- 16.interview study guide/05.attr_accessors.rb
+++ 16.interview study guide/05.attr_accessors.rb
@@ -24,10 +24,11 @@ bob.name = 'Nathan'
 p bob.name
 
 
-# If we want to access an instance variable, we have to create a method
-# that will return that instance variable.
+# If we want to access an instance variable, we have
+# to create a method that will return that instance 
+# variable.
 
 # Getter and Setter methods take up a lot of room.
 
-# Ruby provides the attr_accessor method to automatically create getter
-# and setter methods for us.
+# Ruby provides the attr_accessor method to 
+# automatically create getter and setter methods for us.
