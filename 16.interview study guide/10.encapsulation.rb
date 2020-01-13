9ee4999eca55552422a313350cb0960ee85dd5fc
Nathan Worden <worden.nathan@gmail.com>
2020-01-13 09:01:50 -0800

    Takedown references to test questions

--- 16.interview study guide/10.encapsulation.rb
+++
@@ -1,9 +0,0 @@
-# Encaosulation lets us hide the internal representation
-# of an object from the outside and only exposes the 
-# methods and propeties that users of the object need.
-
-# This is a form of data protection because it means
-# that things can only be changed with obvious intention.
-# We expose properties and methods through the public
-# interface of a class: its public methods.
-
