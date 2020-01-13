86bce0e0e379f218416477bdb75b620c9ef67380
Nathan Worden <worden.nathan@gmail.com>
2020-01-13 08:36:26 -0800

    Redacting the questions so that nothing from the tests are present

--- 01.specific_topics_of_interest/q15.rb
+++ 01.specific_topics_of_interest/q15.rb
@@ -1,14 +1,1 @@
-class Course
-  def initialize(teachers, students)
-    @teachers = teachers
-    @students = students
-  end
-end
-
-class Teacher
-
-end
-
-class Student
-
-end
+redacted
--- 01.specific_topics_of_interest/q9.rb
+++ 01.specific_topics_of_interest/q9.rb
@@ -1,16 +1,1 @@
-class Person
-  attr_accessor :name
-
-  def initialize(n)
-    @name = n
-  end
-
-  def change_info(new_name)
-    @name = new_name         # invoking the setter method
-  end
-end
-
-bob = Person.new('bob')
-bob.change_info('Robert')
-
-puts bob.name
+redacted
